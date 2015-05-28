69380fcd0157eac38c3c0a99863db3928f564b59
Paul Brescia <brescia717@gmail.com>
2015-05-28 16:23:47 -0400

    1. Search now uses AJAX 2. Moved _search partial to _cards partial. Deletion of _search partial pending. 3. Removed render search partial from cards/index.html.erb

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -4,4 +4,14 @@ $(function() {
     $.get(this.href, null, null, "script");
     return false;
   });
+  $(".card_search submit").live("click", function() {
+    event.preventDefault;
+    $.get(this.href, null, null, "script");
+    return false;
+  });
+  // $(".sort a .sort_link").live("click", function() {
+  //   event.preventDefault;
+  //   $.get(this.href, null, null, "script");
+  //   return false;
+  // });
 });
