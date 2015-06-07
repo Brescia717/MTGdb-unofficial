a9a0c7c1ca1419688d63b97509ee9af54379d944
Paul Brescia <brescia717@gmail.com>
2015-06-07 14:36:30 -0400

    Added link to TCGPlayer shop search results

--- app/models/card.rb
+++ app/models/card.rb
@@ -31,4 +31,13 @@ class Card < ActiveRecord::Base
   def fetch_image
     "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=#{self.multiverseid}&type=card"
   end
+
+  def fetch_price
+    set_name_param  = self.card_set if self.card_set
+    card_name_param = self.name if self.name
+    replacements    = [ [/\s/, "_"], [/,\s/, ",_"] ]
+    replacements.each {|r| card_name_param.gsub!(r[0], r[1]) }
+    replacements.each {|r| set_name_param.gsub!(r[0], r[1])  }
+    "http://shop.tcgplayer.com/magic/product/show?ProductName=#{card_name_param}&newSearch=true"
+  end
 end
