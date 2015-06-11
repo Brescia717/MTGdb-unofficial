54fe6a0d6efee71f14548725b7f588d282917a15
Paul Brescia <brescia717@gmail.com>
2015-06-11 12:33:04 -0400

    1. Made card.name a link to the card on _cards partial 2. Modified logic for url creation in the get_price method in cards controller 3. Added user_tag method to deck model 4. Added 'created by' field to deck index and show views

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -20,10 +20,10 @@ class CardsController < ApplicationController
   def get_price(card)
     set_name_param  = card.card_set if card.card_set
     card_name_param = card.name if card.name
-    replacements    = [ [/,\s/, "+"], [/\s/, "+"] ]
-    replacements.each {|r| card_name_param.gsub!(r[0], r[1]) }
-    replacements.each {|r| set_name_param.gsub!(r[0], r[1])  }
-    @url = "http://www.mtggoldfish.com/price/#{set_name_param}/#{card_name_param}#online"
+    replacements    = [ [/,\s/, "+"], [/[\']/, ""] ]
+    replacements.each { |r| card_name_param.gsub!(r[0], r[1]) }
+    replacements.each { |r| set_name_param.gsub!(r[0], r[1]) }
+    @url = "http://www.mtggoldfish.com/price/#{CGI.escape(set_name_param)}/#{CGI.escape(card_name_param)}#online"
     uri = URI.parse(@url)
     req = Net::HTTP.new(uri.host, uri.port)
     res = req.request_head(uri.path)
@@ -32,7 +32,7 @@ class CardsController < ApplicationController
       paper_price = "$#{doc.at_css('div.price-box.paper div.price-box-price').text}"
     else
       paper_price = "Price not found."
-      @url = '#'
+      # @url = '#'
     end
     paper_price
   end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -3,4 +3,8 @@ class Deck < ActiveRecord::Base
   has_many   :cards
 
   serialize :library, JSON
+
+  def user_tag
+    self.user.email.gsub(/@+\w+.+\z/, '')
+  end
 end
