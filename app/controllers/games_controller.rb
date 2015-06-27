6404ecf9acddb68c3b8e11c46250a6274a2ab4a0
Paul Brescia <brescia717@gmail.com>
2015-06-27 12:15:01 -0400

    Modifying controllers to get mulligan to work

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,8 +5,25 @@ class GamesController < ApplicationController
   def show
     @deck      = Deck.find(params[:id])
     @library   = @deck.deck_data
-    @hand    ||= []
-    @game      = Game.new(@library, @hand)
-    @starting_hand = @game.mulligan.hand
+    # binding.pry
+    if params[:mulligan]
+      if session[:hand].nil?
+        session[:hand] ||= []
+        @game = Game.new(@library, session[:hand])
+        session.delete(:hand)
+        session[:hand] = @game.mulligan.hand
+        @hand = session[:hand]
+        binding.pry
+      else
+        @game = Game.new(@library, session[:hand])
+        session.delete(:hand)
+        session[:hand] = @game.mulligan.hand
+        @hand = session[:hand]
+        if @hand.size < 1
+          session.delete(:hand)
+        end
+        binding.pry
+      end
+    end
   end
 end
