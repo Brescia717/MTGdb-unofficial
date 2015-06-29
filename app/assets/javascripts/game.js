e6bf13cbd6ed418013a5fd20e42130c06bfa97c0
Paul Brescia <brescia717@gmail.com>
2015-06-29 13:40:14 -0400

    Added tap/untap feature for cards in play

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,22 +1,37 @@
 $(document).ready(function() {
   "use strict";
-  $(".card").addClass(function(index){
+
+  $(".card").addClass(function(index) {
     return "index-" + index;
   });
 
   $(".card").on('click', function() {
     var card = $(this);
-    if (card.hasClass("hand") == true) {
+
+    if (card.hasClass("hand") === true) {
       card.removeClass("hand");
       card.addClass("in_play");
       $('#battlefield').append( card );
-    } else if (card.hasClass("in_play") == true) {
-      card.rotate(90);
-    };
-    if (card.hasClass("library") == true) {
+    }
+
+    if (card.hasClass("in_play")) {
+      card.addClass("untapped");
+      card.on('click', function() {
+        card.toggleClass("tapped");
+        card.toggleClass("untapped");
+      });
+      if (card.hasClass('tapped')) {
+        TweenMax.to(".tapped img", 0.5, {rotation:90});
+      } else {
+        TweenMax.to(".untapped img", 0.5, {rotation:0});
+      }
+    }
+
+    if (card.hasClass("library") === true) {
       card.removeClass("library");
       card.addClass("hand");
       $("#hand").append( card );
     }
+
   });
 });
