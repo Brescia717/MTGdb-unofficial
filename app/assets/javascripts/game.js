5c250ad869aa713e5f25412e204591c437270723
Paul Brescia <brescia717@gmail.com>
2015-07-01 17:19:08 -0400

    1. Added styling for games/show 2. Deck now draws from first span el instead of last span el

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,49 +1,37 @@
 $(document).ready(function() {
-  "use strict";
+  'use strict';
 
-  $(".card").addClass(function(index) {
-    return "index-" + index;
+  $('.card').addClass(function(index) {
+    return 'index-' + index;
   });
 
-  $(".card").on('click', function() {
+  $('.card').on('click', function() {
     var card = $(this);
 
-    if (card.hasClass("hand") === true) {
-      card.removeClass("hand");
-      card.addClass("in_play");
+    if (card.hasClass('hand') === true) {
+      card.removeClass('hand');
+      card.addClass('in_play');
       $('#battlefield').append( card );
     }
 
-    if (card.hasClass("in_play")) {
-      card.addClass("untapped");
+    if (card.hasClass('in_play')) {
+      card.addClass('untapped');
       card.on('click', function() {
-        if (card.hasClass("untapped")) {
-          card.addClass("tapped");
-          card.removeClass("untapped");
-          TweenMax.to(".tapped img", 0.25, {rotation:90});
-        } else if (card.hasClass("tapped")) {
-          card.addClass("untapped");
-          card.removeClass("tapped");
-          TweenMax.to(".untapped img", 0.25, {rotation:0});
+        if (card.hasClass('untapped')) {
+          card.addClass('tapped');
+          card.removeClass('untapped');
+          TweenMax.to('.tapped img', 0.25, {rotation:90});
+        } else if (card.hasClass('tapped')) {
+          card.addClass('untapped');
+          card.removeClass('tapped');
+          TweenMax.to('.untapped img', 0.25, {rotation:0});
         }
       });
     }
-
-    if (card.hasClass("library") === true) {
-      card.removeClass("library");
-      card.addClass("hand");
-      $("#hand").append( card );
-    }
   });
-  // var i = 1;
-  // if (i < 60) {
-  //   var topCard = $('#library span:nth-last-child('+ i +')')
-  //   $('#draw_button').on('click', function() {
-  //     topCard.removeClass("library");
-  //     topCard.addClass("hand");
-  //     $("#hand").append( topCard );
-  //   });
-  //   i = i + 1;
-  // }
+
+  $('#draw_button').on('click', function() {
+    $('#library span').first().appendTo('#hand').removeClass('library').addClass('hand');
+  });
 
 });
