0504a36def502e300f33f5b64da4a2aecbe1288a
Paul Brescia <brescia717@gmail.com>
2015-08-05 14:26:32 -0400

    1. Added custom context menu when right clicking on cards 2. Added styling for graveyard box

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     return 'index-' + index;
   });
   $('.card').draggable();
+  $('.card').addClass('clickable');
   $('.card').on('click', function() {
     var card = $(this);
 
@@ -28,6 +29,65 @@ $(document).ready(function() {
 
   $('#draw_button').on('click', function() {
     $('#library span').first().appendTo('#hand').removeClass('library').addClass('hand');
+  });
+
+  // Trigger action when the contexmenu is about to be shown
+  $('.clickable').on("contextmenu", function (event) {
+
+      // Avoid the real one
+      event.preventDefault();
+
+      // Show contextmenu
+      $(".custom-menu").finish().toggle(100).
+
+      // In the right position (the mouse)
+      css({
+          top: event.pageY + "px",
+          left: event.pageX + "px"
+      });
+  });
+
+
+  // If the document is clicked somewhere
+  $(document).on("mousedown", function (e) {
+
+      // If the clicked element is not the menu
+      if (!$(e.target).parents(".custom-menu").length > 0) {
+
+          // Hide it
+          $(".custom-menu").hide(100);
+      }
+  });
+
+  $('.clickable').on('contextmenu', function() {
+    var card = $(this);
+    // If the menu element is clicked
+    $(".custom-menu li").on('click', function(){
+
+      // This is the triggered action name
+      switch($(this).attr("data-action")) {
+
+          // A case for each action. Your actions here
+          case "first":
+            card.
+            removeClass('untapped tapped in_play hand graveyard').
+            addClass('graveyard').
+            appendTo('#graveyard');
+            TweenMax.to('.graveyard img', 0.25, {rotation:90});
+            break;
+          case "second":
+            card.
+            removeClass('tapped untapped in_play hand graveyard').
+            addClass('hand').
+            appendTo('#hand');
+            TweenMax.to('.hand img', 0.25, {rotation:0});
+            break;
+          // case "third": $('.me').prependTo('.gy'); break;
+      }
+
+      // Hide it AFTER the action was triggered
+      $(".custom-menu").hide(100);
+    });
   });
 
 });
