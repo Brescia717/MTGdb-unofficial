bcdfd0809b087a60189bcbcccc3ee0b7542504ce
Paul Brescia <brescia717@gmail.com>
2015-11-19 18:48:49 -0500

    Updated tests

--- spec/features/card_search_spec.rb
+++ spec/features/card_search_spec.rb
@@ -5,14 +5,18 @@ feature "User uses Card Search", js: true do
 
   background do
     visit cards_path
-  end
-
-  scenario "successfully" do
     expect(page).to have_content("Search for cards you would like")
     fill_in("cards_autocomplete_name", with: card.name)
     click_on "card-search-submit"
 
     expect(page).to have_css("#cards-table td#card-name")
-    expect(page.first('#cards-table td#card-name').text).to eq("Cryptic Command")
+  end
+
+  scenario "and finds card successfully" do
+    expect(page.first('#cards-table td#card-name').text).to eq(card.name)
+  end
+
+  scenario "and does not find card" do
+    expect(page.first('#cards-table td#card-name').text).not_to eq("Incendiary Command")
   end
 end
