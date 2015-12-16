bb28025ec80893814d1f922e49cfb38f62aa7128
Paul Brescia <brescia717@gmail.com>
2015-12-16 10:35:39 -0500

    Refactored some of the code for controllers and models; modified whenever job

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
-# See https://github.com/sstephenson/execjs#readme for more supported runtimes
 gem 'therubyracer'#, platforms: :ruby
 gem 'jquery-rails', '~> 4.0.3'
 gem 'jquery-ui-rails'
@@ -34,17 +33,10 @@ gem 'paypal-sdk-rest'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
-
 group :development, :test do
   gem 'pry-rails'
-  # gem 'pry-byebug'
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
-  # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'paperclip', '~> 4.2'
   gem 'rmagick'
--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -21,29 +21,11 @@ class CardsController < ApplicationController
     @comment     = Comment.new
   end
 
-private
+
+  private
 
   def card_params
     params[:card].permit!
   end
 
-  def get_price(card)
-    set_name_param  = card.card_set if card.card_set
-    card_name_param = card.name if card.name
-    replacements    = [ [/,\s/, "+"], [/[\']/, ""] ]
-    replacements.each { |r| card_name_param.gsub!(r[0], r[1]) }
-    replacements.each { |r| set_name_param.gsub!(r[0], r[1]) }
-    @url = "http://www.mtggoldfish.com/price/#{CGI.escape(set_name_param)}/#{CGI.escape(card_name_param)}#online"
-    uri = URI.parse(@url)
-    req = Net::HTTP.new(uri.host, uri.port)
-    res = req.request_head(uri.path)
-    if (res.code == "200")
-      doc = Nokogiri::HTML(open(@url))
-      paper_price = "$#{doc.at_css('div.price-box.paper div.price-box-price').text}"
-    else
-      paper_price = "Price not found."
-      @url = '#'
-    end
-    paper_price
-  end
 end
